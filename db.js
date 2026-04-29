7530addfd9f23089c45d27a868d4e8327639d1c3
ishayasin769 <ishayasin769@gmail.com>
2026-04-29 17:33:21 +0500

    folder structure completed

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    user: "postgres",
+    host: "localhost",
+    database: "myTask",
+    password: "admin",
+    port: 5432
 });
 
 module.exports = pool;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-const pool = require('../db');
+const pool = require('../../db');
 
 // CREATE USER
 exports.createUser = async (req, res) => {
